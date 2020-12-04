5f5dcabc82ad9ea52d33ee5b7fc6a7ded5e81362
Hung V Le <leviethung041_140@yahoo.com>
2020-12-04 03:06:03 -0800

    fix profile

--- src/components/updateprofile.js
+++ src/components/updateprofile.js
@@ -1,8 +1,4 @@
 async function editProfile() {
-    console.log("updateProfile-start")
-    await updateProfile()
-    console.log("updateProfile-end")
-
     await checkProfile()
     var profile_data = getUserProfile()
     console.log("editProfile " + profile_data)
@@ -11,14 +7,21 @@ async function editProfile() {
     console.log("editProfile " + jQuery.isEmptyObject(JSON.parse(profile_data)).toString())
 
     if (jQuery.isEmptyObject(JSON.parse(profile_data)) === true) {
-        window.location = '/profile.html';
+        console.log("createProfile-start")
+        await createProfile()
+        console.log("createProfile-end")
     } else {
-        console.log("editProfile" + profile_data)
-        window.location = '/home.html';
+        console.log("updateProfile-start")
+        await updateProfile()
+        console.log("updateProfile-end")
     }
+    await checkProfile()
+    profile_data = getUserProfile()
+    console.log("editProfile" + profile_data)
+    window.location = '/home.html';
 }
 
-async function updateProfile() {
+async function createProfile() {
     var defaultRate = 20;
     var defaultRole = "employee";
     console.log("updateProfile " + document.getElementById('email').value)
@@ -41,6 +44,55 @@ async function updateProfile() {
                 ":fn": $('#firstname').val(),
                 ":ln": $('#lastname').val(),
                 ":rl": defaultRole,
+                ":sa": $('#street').val(),
+                ":ct": $('#city').val(),
+                ":st": $('#state').val(),
+                ":z": $('#zip').val()
+            },
+            "ReturnValues": "ALL_NEW"
+        }
+    }
+
+    var additionalParams = {
+        headers: {
+
+        },
+        queryParams: {
+            'emailid': $('#email').val()
+        }
+    }
+    await apigClient.payrolldaoPost(params, body, additionalParams)
+        .then(function (response) {
+            console.log(response.data)
+            var dataResult = response.data
+            console.log(jQuery.isEmptyObject(dataResult).toString())
+            console.log(JSON.stringify(dataResult))
+        }).catch(function (error) {
+            console.log(error)
+        })
+}
+
+async function updateProfile() {
+    var defaultRate = 20;
+    var defaultRole = "employee";
+    console.log("updateProfile " + document.getElementById('email').value)
+    console.log("updateProfile " + $('#firstname').val())
+
+    var params = {
+        'emailid': $('#email').val()
+    }
+    var body =
+    {
+        "operation": "update",
+        "payload": {
+            "TableName": "Payroll",
+            "Key": {
+                "emailid": $('#email').val()
+            },
+            "UpdateExpression": "set firstname =:fn, lastname =:ln, streetaddress =:sa, city =:ct, state_province =:st, zip =:z",
+            "ExpressionAttributeValues": {
+                ":fn": $('#firstname').val(),
+                ":ln": $('#lastname').val(),
                 ":sa": $('#street').val(),
                 ":ct": $('#city').val(),
                 ":st": $('#state').val(),
